ddd9288b69bc8c203145e309017206b66f2acefd
Kishan Kumar <106915217+Kishan015@users.noreply.github.com>
2022-06-15 18:02:03 +0530

    Create 02 Permutation.java

---
+++ 04 Recursion_and_Backtracking Day10/permutation/02 Permutation.java
@@ -0,0 +1,31 @@
+ class Solution {
+        // TC - O(n * n!)
+        // SC - O(n!)
+        public List<List<Integer>> permute(int[] nums) {
+            List<List<Integer>> result = new ArrayList<>();
+            permute(nums, 0, result);
+            return result;
+        }
+
+        private void permute(int[] nums, int index, List<List<Integer>> result) {
+            if (index == nums.length) {
+                List<Integer> copyList = new ArrayList();
+                for (int num : nums)
+                    copyList.add(num);
+                result.add(copyList);
+                return;
+            }
+
+            for (int i = index; i < nums.length; i++) {
+                swap(nums, i, index);
+                permute(nums, index + 1, result);
+                swap(nums, i, index); // backtrack
+            }
+        }
+
+        private void swap(int[] nums, int i, int j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
