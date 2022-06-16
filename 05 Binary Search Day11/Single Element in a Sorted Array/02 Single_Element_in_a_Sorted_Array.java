0b6f8afa98e4f2c93905e42bbdb792068832376a
Kishan Kumar <106915217+Kishan015@users.noreply.github.com>
2022-06-16 14:40:27 +0530

    Create 02 Single_Element_in_a_Sorted_Array.java

---
+++ 05 Binary Search Day11/Single Element in a Sorted Array/02 Single_Element_in_a_Sorted_Array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int l = 0;
+        int r = nums.length -1 ;
+        while ( l< r ){
+            int mid = l + (r-l)/ 2;
+            if( mid % 2 == 0){
+                if( nums[mid] == nums [mid +1])
+                    l = mid +2;
+                else if(nums[ mid] == nums[ mid -1])
+                    r = mid -2;
+                else 
+                    return nums[mid];
+            }else {
+                if (nums[ mid] == nums[mid -1])
+                    l = mid+ 1;
+                else 
+                    r = mid -1;
+            }
+        }
+        return nums[l];
+    }
+}
