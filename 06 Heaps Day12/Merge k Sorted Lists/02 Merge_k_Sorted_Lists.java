5cfb3055879eb7ca8c55965f9748c68b3db0245d
Kishan Kumar <106915217+Kishan015@users.noreply.github.com>
2022-06-17 15:00:19 +0530

    Create 02 Merge_k_Sorted_Lists.java

---
+++ 06 Heaps Day12/Merge k Sorted Lists/02 Merge_k_Sorted_Lists.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    // TC:  Klogk
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length ==0){
+            return null;
+        }
+
+        Comparator<ListNode> customCompare = new Comparator<ListNode>(){
+            @Override
+            public int compare(ListNode a, ListNode b){
+                return a.val -b.val;
+            }
+        };
+
+        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode> (customCompare);
+
+        for(ListNode list : lists){
+            if(list !=null){
+                pq.offer(list);
+            }
+        }
+
+
+        ListNode dummy = new ListNode(-1);
+        ListNode t = dummy;
+
+        while(!pq.isEmpty()){
+            ListNode lowesetValNode = pq.poll();
+            t.next = lowesetValNode;
+            t   =   t.next;
+
+            if(lowesetValNode.next!=null){
+                pq.offer(lowesetValNode.next);
+            }
+        }
+
+        return dummy.next;
+
+    }
+}
+
